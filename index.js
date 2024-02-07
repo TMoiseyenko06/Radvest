7e3c5be9f074ea11fe864ae817de00c86e270817
TMoiseyenko06 <140467502+TMoiseyenko06@users.noreply.github.com>
2024-02-07 12:19:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const maxResults = 4;
 function embedLatestVideos(videos) {
     const videosContainer = document.getElementById('videos-container');
 
+    // Sort videos by publishedAt in descending order
+    videos.sort((a, b) => new Date(b.snippet.publishedAt) - new Date(a.snippet.publishedAt));
+
+    // Clear the container before adding new iframes
+    videosContainer.innerHTML = '';
+
     videos.forEach(video => {
         const iframe = document.createElement('iframe');
         iframe.src = `https://www.youtube.com/embed/${video.id.videoId}`;
@@ -16,6 +22,7 @@ function embedLatestVideos(videos) {
         videosContainer.appendChild(iframe);
     });
 }
+
 
 async function getLatestVideos() {
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&order=date&maxResults=${maxResults}&key=${apiKey}`;
@@ -35,4 +42,47 @@ async function getLatestVideos() {
 }
 
 
+function createDivForEachObject(data) {
+    var container = document.getElementById('container'); 
+    var vid = JSON.parse(s);
+    data.forEach(function(obj) {
+
+      var name = vid.name;
+      var preacher = vid.preacher;
+      var description = vid.description;
+      var link = vid.videoLink;
+      var date = vid.date;
+      var img = vid.imgPath;
+
+      var div = document.createElement('div');
+      
+      var nameElement = document.createElement('h1');
+      nameElement.textContent = name;
+      var dateElement = document.createElement('p');
+      dateElement.textContent = date;
+      
+      
+      div.appendChild(nameElement);
+      
+      
+      container.appendChild(div);
+    });
+  }
+  
+  
+  fetch('1.json')
+    .then(response => response.json()) 
+    .then(jsonData => {
+      createDivForEachObject(jsonData); 
+    })
+    .catch(error => {
+      console.error('Error fetching JSON: ', error);
+    });
+  
+
+
 getLatestVideos();
+
+
+
+
